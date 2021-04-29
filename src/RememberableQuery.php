b8ff446c30dc0b59c0c185fae377d74c3e01399b
DarkGhosthunter <DarkGhostHunter@gmail.com>
2021-04-29 00:56:09 -0400

    Version 2.0

--- src/RememberableQuery.php
+++ src/RememberableQuery.php
@@ -3,9 +3,10 @@
 namespace DarkGhostHunter\RememberableQuery;
 
 use Illuminate\Support\Traits\ForwardsCalls;
-use Illuminate\Contracts\Cache\Repository as Cache;
-use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Contracts\Cache\Repository;
+use Illuminate\Database\Query\Builder;
 use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use RuntimeException;
 
 class RememberableQuery
 {
@@ -16,7 +17,7 @@ class RememberableQuery
      *
      * @var \Illuminate\Contracts\Cache\Repository
      */
-    protected $cache;
+    protected Repository $cache;
 
     /**
      * Query Builder instance
@@ -35,9 +36,9 @@ class RememberableQuery
     /**
      * The Cache Key to use
      *
-     * @var string
+     * @var string|null
      */
-    protected $cacheKey;
+    protected ?string $cacheKey = null;
 
     /**
      * RememberableQuery constructor.
@@ -45,20 +46,10 @@ class RememberableQuery
      * @param  \Illuminate\Contracts\Cache\Repository $cache
      * @param \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Query\Builder
      */
-    public function __construct(Cache $cache, $builder)
+    public function __construct(Repository $cache, $builder)
     {
         $this->cache = $cache;
         $this->builder = $builder;
-    }
-
-    /**
-     * Returns the Builder instance
-     *
-     * @return \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Query\Builder
-     */
-    public function builder()
-    {
-        return $this->builder;
     }
 
     /**
@@ -68,7 +59,7 @@ class RememberableQuery
      * @param  string|null $cacheKey
      * @return $this
      */
-    public function remember($ttl, string $cacheKey = null)
+    public function remember($ttl, string $cacheKey = null): RememberableQuery
     {
         $this->ttl = $ttl;
 
@@ -85,7 +76,7 @@ class RememberableQuery
      * @return mixed
      * @throws \Psr\SimpleCache\InvalidArgumentException
      */
-    public function __call($method, $arguments)
+    public function __call(string $method, array $arguments)
     {
         // First, get the Cache Key we will work with.
         $key = $this->cacheKey();
@@ -95,15 +86,17 @@ class RememberableQuery
             return $this->cache->get($key);
         }
 
-        // Since we don't have any result, let's call the Query Builder.
+        // Since we don't have any result, get it from the Query Builder.
         $result = $this->forwardCallTo($this->builder, $method, $arguments);
 
-        // If the call returns the same Builder instance, just return this to keep building the Query.
-        if ($result instanceof QueryBuilder || $result instanceof EloquentBuilder) {
-            return $this;
+        // Force the developer to use this as before-last method in the query builder.
+        if ($result instanceof Builder || $result instanceof EloquentBuilder) {
+            throw new RuntimeException(
+                "The `remember()` method call is not before query execution: [$method] called."
+            );
         }
 
-        // Otherwise, we will have a result from the Query. In that case, we will save it before returning.
+        // Save the result before returning it.
         $this->cache->put($key, $result, $this->ttl);
 
         return $result;
@@ -126,6 +119,7 @@ class RememberableQuery
      */
     public function cacheKeyHash() : string
     {
-        return 'query|' . md5($this->builder->toSql() . implode('', $this->builder->getBindings()));
+        return 'query|' .
+            base64_encode(md5($this->builder->toSql() . implode('', $this->builder->getBindings()), true));
     }
 }
--- src/RememberableQueryServiceProvider.php
+++ src/RememberableQueryServiceProvider.php
@@ -2,9 +2,9 @@
 
 namespace DarkGhostHunter\RememberableQuery;
 
-use Illuminate\Support\ServiceProvider;
-use Illuminate\Database\Query\Builder as QueryBuilder;
 use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Support\ServiceProvider;
 
 class RememberableQueryServiceProvider extends ServiceProvider
 {
@@ -13,14 +13,24 @@ class RememberableQueryServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(): void
     {
-        QueryBuilder::macro('remember', function ($ttl = 60, string $cacheKey = null) {
-            return app(RememberableQuery::class, ['builder' => $this ])->remember($ttl, $cacheKey);
-        });
+        if (! QueryBuilder::hasMacro('remember')) {
+            QueryBuilder::macro(
+                'remember',
+                function ($ttl = 60, string $cacheKey = null): RememberableQuery {
+                    return app(RememberableQuery::class, ['builder' => $this])->remember($ttl, $cacheKey);
+                }
+            );
+        }
 
-        EloquentBuilder::macro('remember', function ($ttl = 60, string $cacheKey = null) {
-            return app(RememberableQuery::class, ['builder' => $this ])->remember($ttl, $cacheKey);
-        });
+        if (! EloquentBuilder::hasGlobalMacro('remember')) {
+            EloquentBuilder::macro(
+                'remember',
+                function ($ttl = 60, string $cacheKey = null): RememberableQuery {
+                    return app(RememberableQuery::class, ['builder' => $this])->remember($ttl, $cacheKey);
+                }
+            );
+        }
     }
 }
